6876fc8e07245212db0435a8e25eaa8ed3ebc055
eitanbadihi3-tech <eitanbadihi3@gmail.com>
2026-06-07 08:33:05 +0300

    Implement service worker with caching strategy

--- sw.js
+++ sw.js
@@ -1,3 +1,40 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = "lessons-app-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((response) => response || caches.match("./index.html")))
+  );
 });
